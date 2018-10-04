bbca37fcbd07f795615a0ad3b7643543207780b1
JelipeHRNDZ123 <43364248+JelipeHRNDZ123@users.noreply.github.com>
2018-10-03 22:03:43 -0500

    Add files via upload

---
+++ felipe_hernadez_ejercico1.m
@@ -0,0 +1,8 @@
+'Determinar si el numero es par o impar'
+prompt='introduce un numero: '
+N=input(prompt);
+    if rem(N,2)==0
+        fprintf('es par')
+    else
+        fprintf('es impar')
+    end
